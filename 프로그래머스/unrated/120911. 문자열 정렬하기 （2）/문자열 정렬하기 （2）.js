5d59564fbdce35ffe061ad03a9197c42b23ce900
Jo MinKyeong <67677374+minkyeongJ@users.noreply.github.com>
2024-01-04 01:43:16 +0900

    [level unrated] Title: 문자열 정렬하기 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,4 +1,4 @@
 function solution(my_string) {
-    var answer = my_string.toLowerCase().split('').sort((a, b) => a.charCodeAt() - b.charCodeAt()).join('');
+    var answer = my_string.toLowerCase().split('').sort().join('');
     return answer;
 }
